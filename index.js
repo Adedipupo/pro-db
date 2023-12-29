2a69dccd11465dde94fc9edb11ec5ac9851c4acc
Adedipupo Jokanola <jokanolaadedipupo@gmail.com>
2023-12-29 23:05:09 +0100

    wip

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-const express = require('express');
- require('dotenv').config();
- 
-
-
-const app = express();
+const app = require('./app');
 
 const PORT = 4000 || process.env.PORT;
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
 
 app.listen(PORT,() => {
     console.log(`Listening on port ${PORT}`);
