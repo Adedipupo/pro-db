2cc07cd83235a21311e333d0b6a3d5c50a205d38
Adedipupo Jokanola <jokanolaadedipupo@gmail.com>
2024-01-24 11:18:15 +0100

    token implementation

--- controller/user.js
+++ controller/user.js
@@ -1,24 +1,50 @@
-const User = require("../model/user");
-const bcrypt = require("bcryptjs");
-
-
+const User = require('../model/user')
+const bcrypt = require('bcryptjs')
+const { generateToken } = require('../utils/generateToken')
 
 const registerUser = async (req, res) => {
-    const {firstname , lastname, email, password } = req.body;
+  try {
+    const { firstname, lastname, email, password } = req.body
 
-    if (!(firstname && lastname && email && password)){
-        res.status(400).send({message: "All fields are required"});
+    if (!(firstname && lastname && email && password)) {
+      res.status(400).send({ message: 'All fields are required' })
     }
 
-    const userExists =  await User.findOne({email});
+    const userExists = await User.findOne({ email })
 
     if (userExists) {
-        res.status(200).send({message: "User already exists"});
+      res.status(200).send({ message: 'User already exists' })
     }
 
+    const user = await User.create({
+      firstname,
+      lastname,
+      email: email.toLowerCase(),
+      password,
+    })
 
-};
+    await newUser.save()
+
+    const token = await generateToken(user._id)
+
+    user.password = undefined
+
+    //Store cookie in the request body.
+    res.cookie('token', token)
+    return res.status(201).json({
+      message: 'User successfully created!!!',
+      data: newUser,
+      token: token,
+    })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      message: 'Internal Server Error',
+      success: false,
+    })
+  }
+}
 
 module.exports = {
-    registerUser
+  registerUser,
 }
---
+++ utils/generateToken.js
@@ -0,0 +1,12 @@
+import jwt from 'jsonwebtoken';
+
+
+  export function generateToken(id) {
+    return jwt.sign({id}, process.env.JWT_SECRET, { expiresIn: '1d' })
+  }
+  export function verifyToken(token) {
+    return jwt.verify(token, process.env.JWT_SECRET)
+  }
+
+
+
